ca6b119e3f668b6d00ce2be86639e2c578b7d8e5
vincepr <vincepr@gmx.de>
2023-10-01 15:26:59 +0200

    removed tun tap header

--- src/main.rs
+++ src/main.rs
@@ -15,24 +15,24 @@ fn main() -> io::Result<()> {
 
     let mut connections: HashMap<Quad, tcp::Connection> = Default::default();
 
-    let mut nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut nic = tun_tap::Iface::without_packet_info("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
 
     loop {
         // bytes we received
         let nbytes = nic.recv(&mut buf[..])?;
-        // info on the ETHERNET FRAME we got:
-        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        // // info on the ETHERNET FRAME we got:
+        // let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        // let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
 
-        // proto=0x0800->Ipv4-packet | proto=0x86dd->Ipv6-packet
-        if eth_proto != 0x0800 {
-            continue;
-        } // we ignore all but ipv4
-          // eprint!("tun-flags: {:X}, tun-proto: {:x}, || ", _eth_flags, eth_proto);
+        // // proto=0x0800->Ipv4-packet | proto=0x86dd->Ipv6-packet
+        // if eth_proto != 0x0800 {
+        //     continue;
+        // } // we ignore all but ipv4
+        //   // eprint!("tun-flags: {:X}, tun-proto: {:x}, || ", _eth_flags, eth_proto);
 
         // we match against the expected Ipv4-Packet
-        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[..nbytes]) {
             Ok(iph) => {
                 // info on IP-PACKET we got:
                 let src = iph.source_addr();
@@ -45,11 +45,11 @@ fn main() -> io::Result<()> {
 
                 // we match against the expected TCP-Packet
                 let iph_size = iph.slice().len();
-                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + iph_size..nbytes]) {
+                match etherparse::TcpHeaderSlice::from_slice(&buf[iph_size..nbytes]) {
                     Ok(tcph) => {
                         use std::collections::hash_map::Entry;
                         let tcph_size = tcph.slice().len();
-                        let data_start_idx = 4 + iph_size + tcph_size;
+                        let data_start_idx = iph_size + tcph_size;
                         match connections.entry(Quad {
                             src: (src, tcph.source_port()),
                             dst: (dst, tcph.destination_port()),
