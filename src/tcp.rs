b972c57adff0339a519b2af257dd8e559debcc2f
vincepr <vincepr@gmx.de>
2023-10-01 20:05:12 +0200

    work in progress, at checking valid windowds

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,10 +1,10 @@
 use std::io;
 
 pub enum State {
-    Closed,
-    Listen,
+    // Closed,
+    // Listen,
     SynRcvd,
-    // Estab,
+    Estab,
 }
 
 /// Transmission Control Block(TCB). stores all connection records.
@@ -13,6 +13,8 @@ pub struct Connection {
     state: State,
     send: SendSequence,
     recv: RecvSequence,
+    /// the ip header we use to send
+    ip: etherparse::Ipv4Header,
 }
 
 /// state of the Send Sequence - https://www.rfc-editor.org/rfc/rfc793.html#page-19
@@ -80,6 +82,23 @@ impl Connection {
                 up: false,
                 irs: tcph.sequence_number(),
             },
+            ip:  etherparse::Ipv4Header::new(
+                0,
+                64,
+                etherparse::IpTrafficClass::Tcp,
+                [
+                    iph.destination()[0],
+                    iph.destination()[1],
+                    iph.destination()[2],
+                    iph.destination()[3],
+                ],
+                [
+                    iph.source()[0],
+                    iph.source()[1],
+                    iph.source()[2],
+                    iph.source()[3],
+                ],
+            )
         };
 
         // we construct the tcp-header and set its proper flags
@@ -92,28 +111,12 @@ impl Connection {
         syn_ack.acknowledgment_number = c.recv.nxt;
         syn_ack.syn = true;
         syn_ack.ack = true;
+        c.ip.set_payload_len(syn_ack.header_len() as usize + 0);
 
         // we construct the ip-header
-        let mut ip = etherparse::Ipv4Header::new(
-            syn_ack.header_len(),
-            64,
-            etherparse::IpTrafficClass::Tcp,
-            [
-                iph.destination()[0],
-                iph.destination()[1],
-                iph.destination()[2],
-                iph.destination()[3],
-            ],
-            [
-                iph.source()[0],
-                iph.source()[1],
-                iph.source()[2],
-                iph.source()[3],
-            ],
-        );
 
         // kernel does this following checksum for us so no need to actually calculate it:
-        // syn_ack.checksum = syn_ack.calc_checksum_ipv4(&ip, &[])
+        // syn_ack.checksum = syn_ack.calc_checksum_ipv4(&c.ip, &[])
             // .expect("unable to compute checksum!");
 
         // we construct the the headers into buf
@@ -122,11 +125,11 @@ impl Connection {
         // - unwritten.len() -> returns how much is remaining of the buffer
         let unwritten = {
             let mut unwritten = &mut buf[..];
-            ip.write(&mut unwritten);
+            c.ip.write(&mut unwritten);
             syn_ack.write(&mut unwritten);
             unwritten.len()
         };
-        
+
         // dbg_print_incoming_packet(iph, tcph);
         // dbg_print_response_packet(&buf, unwritten);
         nic.send(&buf[..buf.len() - unwritten])?;
@@ -140,11 +143,27 @@ impl Connection {
         tcph: etherparse::TcpHeaderSlice<'a>,
         data: &'a [u8],
     ) -> io::Result<()> {
+        
+        // check if ack we got is ok
+        if !is_valid_ack(tcph.acknowledgment_number(), self.send.una, self.send.nxt) { 
+            return Ok(());
+        }
+        // check if segment is in our range that we accept (window-size in bytes)
+
+
         self.dbg_print_packet(iph, tcph, data);
-        unimplemented!()
+        match self.state {
+            State::SynRcvd => {
+                // we expect an ACK back from the SYN we just sent
+
+                Ok(())
+            },
+            State::Estab => {
+                todo!()
+            },
+        }
     }
-
-
+    
     #[allow(dead_code)]
     fn dbg_print_packet<'a>(
         &mut self,
@@ -164,6 +183,14 @@ impl Connection {
 }
 
 
+// acceptable ack? = SND.UNA < SEG.ACK =< SND.NXT THIS WRAPS ARROUND if hitting max!
+fn is_valid_ack(ack:u32, una:u32, nxt:u32)-> bool{
+    if nxt < una {
+        // special case, nxt has wrapped arround after hitting 32bit.max
+        return ack > una || ack <= nxt;
+    }
+    return una < ack && ack <= nxt;
+}
 
 #[allow(dead_code)]
 fn dbg_print_incoming_packet(
@@ -176,4 +203,40 @@ fn dbg_print_incoming_packet(
 #[allow(dead_code)]
 fn dbg_print_response_packet(buf: &[u8], unwritten: usize) {
     eprintln!("reponding with {:02x?}\n", &buf[..buf.len() - unwritten])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::Rng;
+
+
+    // og implementation, mine makes more sense for myself 
+    fn _is_valid_ack_og(ack:u32, una:u32, nxt:u32)-> bool{
+        if una < ack {
+            if nxt > una && nxt < ack{
+                return false;
+    
+            }
+        } else {
+            if nxt >= ack && nxt < una {
+    
+            } else {
+                return false;
+            }
+        }
+        return true;
+    } 
+
+    #[test]
+    fn test_mine_og_comparison_valid_ack() {
+       for n in 1..10000 {
+        
+        let ack = rand::random::<u32>();
+        let una = rand::random::<u32>();
+        let nxt = rand::random::<u32>();
+        dbg!(ack, una, nxt);
+        assert_eq!(is_valid_ack(ack, una, nxt), _is_valid_ack_og(ack, una, nxt))
+       }
+    }
 }
